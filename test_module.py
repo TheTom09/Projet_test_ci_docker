36e10bcf181533d5d83f8e5fe8ab30c8b46c7302
Thomas Leydet <tomleydet@icloud.com>
2022-04-15 10:34:08 +0200

    test_module.py

--- test_module.py
+++ test_module.py
@@ -29,3 +29,9 @@ def test_Person_isDead_dead():
     for i in range(10):
         person2.hit(person1)
 
+def test_Person_gained_life_points():
+    person1 = Person("Gained")
+    person1.gained_life_points(10)
+    expected_result = 110
+    result = person1.get_life_points()
+    assert result == expected_result
